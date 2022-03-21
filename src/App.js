03faddaeaf239e97bc38f51caf6842464eecf53a
PhanDuyDien <dien3254@gmail.com>
2022-03-22 00:36:50 +0700

    Upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Title from "./final/components/Title";
+import Setup from "./final/examples/5-custom-instance";
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup />
+    </main>
+  );
+}
+
+export default App;
